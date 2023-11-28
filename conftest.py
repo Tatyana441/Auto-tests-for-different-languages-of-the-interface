873d26d4ae1f120ebb68817b3ad91db8dde9d825
Maksim Ivahnenko <ivahnenkomv@anodialog.ru>
2023-11-28 13:41:40 +0300

    Поправил confest.py и тест

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru, en etc.")
@@ -11,7 +13,8 @@ def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart browser for test..")
     browser = webdriver.Chrome(options=options)
-
-
+    yield browser
+    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -7,4 +7,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_check(browser):
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn btn-lg btn-primary btn-add-to-basket")
+    btn = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert btn, 'Cелектор кнопки не найден'
+    print(btn)
+    time.sleep(30)
