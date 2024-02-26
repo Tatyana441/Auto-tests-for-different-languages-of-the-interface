677964f02db5bb008ae6a47197533799b5d1a54b
Tatyana441 <90765665+Tatyana441@users.noreply.github.com>
2024-02-26 11:44:44 +0300

    Update test_items.py
    Изменение названия теста

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_check(browser):
+def test_guest_should_see_cart_button(browser):
     browser.get(link)
     btn = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert btn, 'Cелектор кнопки не найден'
